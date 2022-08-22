ccdc389c9f935ed94e67de62d2311d0f743360e0
wuguangbin <2567199685@qq.com>
2022-08-22 22:54:48 +0800

    deploy

---
+++ precache-manifest.7de6a8f7ae88794ad79513038a1a2dab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb468c879cf1baa4231826238ff3806",
+    "url": "./index.html"
+  },
+  {
+    "revision": "885a172406a3c26eb083",
+    "url": "./static/css/main.b586f6e1.chunk.css"
+  },
+  {
+    "revision": "a75990f892468c0519e7",
+    "url": "./static/js/2.89b26af3.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "./static/js/2.89b26af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "885a172406a3c26eb083",
+    "url": "./static/js/main.b1e92365.chunk.js"
+  },
+  {
+    "revision": "d30be863f9666e894e2c",
+    "url": "./static/js/runtime-main.edd1a70f.js"
+  }
+]);
